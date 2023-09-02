40d2e9155c3f50d6925a132dcdfb482deeacc4a5
natariolucas <lucasenatario@gmail.com>
2023-09-02 13:25:46 -0300

    poc

--- Librerias/tda/pila/tda_pila_estatica.c
+++ Librerias/tda/pila/tda_pila_estatica.c
@@ -1,2 +1,77 @@
 #include "tda_pila_estatica.h"
+#include <string.h>
 
+void crear_pila(pila *pp) {
+    pp->tope = TAM_VEC;
+}
+
+int poner_en_pila(pila* pp, const void* elem, size_t tamElem) {
+    if(pp->tope < tamElem + sizeof(size_t)) {
+        return PILA_LLENA;
+    }
+    //guardo elemento
+    pp->tope -= tamElem;
+    memcpy(pp->vec + pp->tope, elem, tamElem);
+
+    //guardo tamanio del elemento
+    pp->tope -= sizeof(size_t);
+    memcpy(pp->vec + pp->tope, &tamElem, sizeof(size_t));
+    return OK;
+}
+
+int sacar_de_pila(pila* pp, void* elem, size_t tamElem) {
+    if(pp->tope == TAM_VEC) {
+        return PILA_VACIA;
+    }
+
+    //Leo tamanio del elemento guardado
+    size_t tamElemGuardado;
+    memcpy(&tamElemGuardado, pp->vec + pp->tope, sizeof(size_t));
+    pp->tope += sizeof(size_t);
+
+    //Leo y guardo el elemento guardado
+    memcpy(elem, pp->vec + pp->tope, menor(tamElem, tamElemGuardado));
+    pp->tope += tamElemGuardado;
+
+    return OK;
+}
+
+void vaciar_pila(pila *pp) {
+    pp->tope = TAM_VEC;
+}
+
+int pila_vacia(const pila* pp) {
+    if (pp->tope == TAM_VEC)
+        return PILA_VACIA;
+
+    return PILA_NO_VACIA; //Esto quedaria mas lindo con booleanos
+}
+
+int pila_llena(const pila* pp, size_t tamElem) {
+    if (pp->tope < sizeof(size_t) + tamElem) {
+        return PILA_LLENA;
+    }
+
+    return PILA_NO_LLENA; //Esto quedaria mas lindo con booleanos
+}
+
+int ver_tope(const pila *pp, void* elem, size_t tamElem){
+    if (pp->tope == TAM_VEC)
+        return PILA_VACIA;
+
+    //Leo tamanio del elemento guardado
+    int tamElemGuardado;
+    memcpy(&tamElemGuardado, pp->vec + pp->tope, sizeof(size_t));
+
+    //Leo y guardo el elemento guardado
+    memcpy(elem, pp->vec + pp->tope, menor(tamElem, tamElemGuardado));
+
+    return OK;
+}
+
+size_t menor(size_t tam1, size_t tam2) {
+    if (tam1 < tam2)
+        return tam1;
+
+    return tam2;
+}
--- Librerias/tda/pila/tda_pila_estatica.h
+++ Librerias/tda/pila/tda_pila_estatica.h
@@ -1,5 +1,27 @@
 #ifndef TDA_PILA_ESTATICA_H_INCLUDED
 #define TDA_PILA_ESTATICA_H_INCLUDED
 
+#include <stddef.h>
 
+#define TAM_VEC 1024
+#define OK 1
+#define PILA_LLENA 2
+#define PILA_VACIA 3
+#define PILA_NO_VACIA 4
+#define PILA_NO_LLENA 5
+
+typedef struct {
+    char vec[TAM_VEC];
+    unsigned tope;
+} pila;
+
+void crear_pila(pila *pp);
+int poner_en_pila(pila *pp, const void* elem, size_t tamElem);
+int sacar_de_pila(pila *pp, void* elem, size_t tamElem);  //tamElem en este caso es el tamaño disponible en la variable que se va a usar para guardar el elemento
+void vaciar_pila(pila *pp);
+int pila_vacia(const pila *pp);
+int pila_llena(const pila *pp, size_t tamElem);
+int ver_tope(const pila *pp, void* elem, size_t tamElem);
+
+size_t menor(size_t tam1, size_t tam2);
 #endif // TDA_PILA_ESTATICA_H_INCLUDED
